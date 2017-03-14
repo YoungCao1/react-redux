181b16e0d23c1bd504fe58f807f2990c8cc0ba0b
fengchengpu <fengchengpu@iciyun.com>
2017-03-14 10:28:41 +0800

    auto refresh

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
+    'webpack/hot/dev-server',
     'react-hot-loader/patch',
     './src/index'
   ],
